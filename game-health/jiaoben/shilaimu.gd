86dc53c29b610528e35a8cad410b4940bf02d67f
IMercuryQAQ <18814855762@163.com>
2026-05-10 09:34:05 +0800

    合并

---
+++ game-health/jiaoben/shilaimu.gd
@@ -0,0 +1,31 @@
+extends Area2D
+@export var shilaimu_sudu : float = -50
+@export var shilaimu_scene : PackedScene
+@export var shilaimu_xueliang : float = 20
+var is_dead : bool = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if not is_dead:
+		position += Vector2(shilaimu_sudu,0) * delta #delta=1/每秒帧数
+	if position.x < -880:
+		queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		await get_tree().create_timer(0.8).timeout
+		body.game_over()
+
+func _on_area_entered(area: Area2D) -> void: #检测子弹等区域碰撞
+	if area.is_in_group("zidan"):
+		shilaimu_xueliang -= 10
+		area.queue_free() #如果想要贯穿的子弹就删掉这行
+	if area.is_in_group('hanbingjian'):
+		shilaimu_xueliang -=10
+		shilaimu_sudu = shilaimu_sudu * 0.75
+	if shilaimu_xueliang == 0:
+		$AnimatedSprite2D.play("siwang") #如果绿色名称被修改就会报错
+		is_dead = true
+		get_tree().current_scene.score += 1
+		#0.6秒后删除史莱姆节点
+		await get_tree().create_timer(0.6).timeout
+		queue_free()
