d2cfb31667cb3b283dc1919ed9127a997a08ef53
xcrp111 <1612416684@qq.com>
2026-05-23 18:46:34 +0800

    寒冰箭bug修复

--- game-health-2026-05-19/hanbingjian.gd
+++ game-health-2026-05-19/hanbingjian.gd
@@ -12,8 +12,5 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemy"):
-		body.hp -= 10
-		if body.hp <= 0:
-			body.queue_free()
-		self.queue_free()
+	pass
+	
