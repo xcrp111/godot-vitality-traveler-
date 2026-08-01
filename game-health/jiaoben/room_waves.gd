809d6bb439627f3134c408b85b9aadd824cbf5db
xcrp111 <1612416684@qq.com>
2026-08-01 14:32:38 +0800

    8.1
    合并后修改
    菜单更新

--- game-health/jiaoben/room_waves.gd
+++
@@ -1,8 +0,0 @@
-class_name RoomWaves
-extends Node
-## 挂在房间 Area2D 下，定义该房间专属的波次配置。
-## 如果不添加此节点，则使用 EnemySpawner 的全局默认波次。
-
-@export var wave_1: Array[PackedScene] = []
-@export var wave_2: Array[PackedScene] = []
-@export var wave_3: Array[PackedScene] = []
